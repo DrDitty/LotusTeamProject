7f4cff254a30c25c74e5214c0322257a12cdb480
Aaditya Madhwesh <arm0054@auburn.edu>
2018-04-26 20:26:40 -0500

    added all the buttons

--- app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/teamLotus/MainActivity.java
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/teamLotus/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -18,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout myLayout = null;
     ImageView blue_button;
+    ImageView red_button;
+    ImageView yellow_button;
+    ImageView green_button;
+    ImageView red_clicked;
+    ImageView yellow_clicked;
+    ImageView green_clicked;
     ImageView blue_clicked;
     Random mRandom = new Random();
 
@@ -30,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
     long difficulty;
     boolean correct = true;
 
-    /*  TODO: The loop is not waiting for touch from the user. fix that
-        TODO:
-        Todo:
+    /*  TODO: maybe make a loop to keep displaying the patterns
+        TODO: And it only plays again if the user input is correct
+        Todo: otherwise it displays a game over message and sends them back to the main menu
         todo:
      */
 
@@ -44,7 +48,14 @@ public class MainActivity extends AppCompatActivity {
 
         myLayout = (RelativeLayout) findViewById(R.id.myLayout);
         blue_button = (ImageView) findViewById(R.id.blue_button);
+        red_button = (ImageView) findViewById(R.id.red_button);
+        yellow_button = (ImageView) findViewById(R.id.yellow_button);
+        green_button = (ImageView) findViewById(R.id.green_button);
+
         blue_clicked = findViewById(R.id.blue_clicked);
+        red_clicked = findViewById(R.id.red_clicked);
+        yellow_clicked = findViewById(R.id.yellow_clicked);
+        green_clicked = findViewById(R.id.green_clicked);
 
         max = 9;
 
@@ -53,95 +64,153 @@ public class MainActivity extends AppCompatActivity {
         current = 0;
 
 
-                for (;current <= max;current++) {
+        for (;current <= max;current++) {
+
+            myLayout.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    int rand;
+                    rand = mRandom.nextInt(4);
+
+                    int i = 0;
+                    int[] array = new int[max];
 
 
-                    myLayout.setOnTouchListener(new View.OnTouchListener() {
-                        @Override
-                        public boolean onTouch(View view, MotionEvent motionEvent) {
-                            int rand;
-                            rand = mRandom.nextInt(4);
+                    if (correct) {
+                        correct = false;
 
-                            int i = 0;
-                            int[] array = new int[40];
+                        if (i <= current) {
+                            array[i] = rand;
 
-
-                            y = motionEvent.getY();
-
-                            if (correct) {
-                                correct = false;
-
-                                if (i <= current) {
-                                    array[i] = rand;
-
-                                    switch (rand) {
-                                        case 0:
-                                            Toast.makeText(getApplicationContext(), "red", Toast.LENGTH_SHORT).show();
-                                            break;
-                                        case 1:
-                                            Toast.makeText(getApplicationContext(), "yellow", Toast.LENGTH_SHORT).show();
-                                            break;
-                                        case 2:
-                                            Toast.makeText(getApplicationContext(), "green", Toast.LENGTH_SHORT).show();
-                                            break;
-                                        case 3: //blue. change to red when you're done testing
-                                            blue_clicked.setVisibility(View.VISIBLE);
-                                            Toast.makeText(getApplicationContext(), "blue", Toast.LENGTH_SHORT).show();
-                                            break;
-                                        default:
-                                            break;
-
-                                    }
-                                    if (blue_clicked.getVisibility() == View.VISIBLE) {
-
-                                        mHandler.postDelayed(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                blue_clicked.setVisibility(View.INVISIBLE);
-                                            }
-                                        }, difficulty);
-                                    }
-
-                                }
-
-                                i++;
-                            }
-
-
-
-                            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                                x = motionEvent.getX();
-
-                                if (x > 778 && x < 1337) {
-
-                                    correct = true;
-                                    current++;
-
-                                    Toast.makeText(getApplicationContext(), String.valueOf(x), Toast.LENGTH_SHORT).show();
+                            switch (rand) {
+                                case 0:
+                                    red_clicked.setVisibility(View.VISIBLE);
+                                    break;
+                                case 1:
+                                    yellow_clicked.setVisibility(View.VISIBLE);
+                                    break;
+                                case 2:
+                                    green_clicked.setVisibility(View.VISIBLE);
+                                    break;
+                                case 3: //blue. change to red when you're done testing
                                     blue_clicked.setVisibility(View.VISIBLE);
-
-                                    mHandler.postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            blue_clicked.setVisibility(View.INVISIBLE);
-
-
-                                        }
-                                    }, 500);
-
-                                }
+                                    break;
+                                default:
+                                    break;
 
                             }
+                            if (blue_clicked.getVisibility() == View.VISIBLE || green_clicked.getVisibility() == View.VISIBLE || red_clicked.getVisibility() == View.VISIBLE || yellow_clicked.getVisibility() == View.VISIBLE ) {
 
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        blue_clicked.setVisibility(View.INVISIBLE);
+                                        red_clicked.setVisibility(View.INVISIBLE);
+                                        yellow_clicked.setVisibility(View.INVISIBLE);
+                                        green_clicked.setVisibility(View.INVISIBLE);
+                                    }
+                                }, difficulty);
+                            }
 
-
-                            return true;
                         }
-                    });
+
+                        i++;
+                    }
+
+                    if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                        x = motionEvent.getX();
+                        y = motionEvent.getY();
+
+                        //Toast.makeText(getApplicationContext(), String.valueOf(y), Toast.LENGTH_SHORT).show();
+
+                        //if blue is released
+                        if (x > 700 && x < 1240) {
+                            if (y > 1145 && y < 1700) {
+
+                                correct = true;
+                                current++;
+
+                                blue_clicked.setVisibility(View.VISIBLE);
+
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        blue_clicked.setVisibility(View.INVISIBLE);
+
+
+                                    }
+                                }, 500);
+                            }
+                        }
+                        //if green is released
+                        if (x > 140 && x < 699) {
+                            if (y > 1145 && y < 1700) {
+                                correct = true;
+                                current++;
+
+                                green_clicked.setVisibility(View.VISIBLE);
+
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        green_clicked.setVisibility(View.INVISIBLE);
+
+
+                                    }
+                                }, 500);
+
+                            }
+                        }
+                        //red
+                        if (x > 140 && x < 699) {
+                            if (y > 590 && y < 1144) {
+                                correct = true;
+                                current++;
+
+                                red_clicked.setVisibility(View.VISIBLE);
+
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        red_clicked.setVisibility(View.INVISIBLE);
+
+
+                                    }
+                                }, 500);
+
+                            }
+                        }
+                        //yellow
+                        if (x > 700 && x < 1240) {
+                            if (y > 590 && y < 1144) {
+                                correct = true;
+                                current++;
+
+                                yellow_clicked.setVisibility(View.VISIBLE);
+
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        yellow_clicked.setVisibility(View.INVISIBLE);
+
+
+                                    }
+                                }, 500);
+
+                            }
+                        }
+
+                    }
 
 
 
+                    return true;
                 }
+            });
+
+
+
+        }
 
 
 
