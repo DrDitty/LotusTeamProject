6faf69cbf09638afe22a00e92d356e7d5a3da8c2
Aaditya Madhwesh <arm0054@auburn.edu>
2018-05-03 16:23:26 -0500

    running tests 4:23

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/spring2018/teamLotus/HomeActivity.java
@@ -0,0 +1,58 @@
+package edu.auburn.eng.csse.comp3710.spring2018.teamLotus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final Button easy = findViewById(R.id.easy_button);
+        easy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_e = new Intent(HomeActivity.this, MainActivity.class);
+                myIntent_e.putExtra("speed", 10);
+                HomeActivity.this.startActivity(myIntent_e);
+            }
+        });
+        final Button medium = findViewById(R.id.medium_button);
+        medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_m = new Intent(HomeActivity.this, MainActivity.class);
+                myIntent_m.putExtra("speed", 20);
+                HomeActivity.this.startActivity(myIntent_m);
+            }
+        });
+        final Button expert = findViewById(R.id.expert_button);
+        expert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent_exp = new Intent(HomeActivity.this, MainActivity.class);
+                myIntent_exp.putExtra("speed", 30);
+                HomeActivity.this.startActivity(myIntent_exp);
+            }
+        });
+
+        final ToggleButton mute = findViewById(R.id.sound_toggle);
+
+    }
+
+    protected void onPause(Bundle savedInstanceState) {
+        super.onPause();
+    }
+
+    protected void onResume(Bundle savedInstanceState) {
+        super.onResume();
+    }
+
+}
